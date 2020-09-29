8246f4f29efb84f909dcdeb37ef974d81b52a8b1
helping083 <helping083@gmail.com>
2020-09-29 11:11:08 +0300

    init store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import {Excel, Header, Table, Formula, Toolbar} from '@/components/';
 import './scss/index.scss';
+import {rootReducer} from './store/rootReducer'
+import {createStore} from './core/createStore';
+
+const store = createStore(rootReducer)
 
 const excel = new Excel('#app', {
   components: [Header, Toolbar, Formula, Table],
+  store,
 });
 excel.render();
---
+++ src/store/rootReducer.js
@@ -0,0 +1,9 @@
+/**
+ * root store of our app
+ * @param {object} state
+ * @param {object} action
+ * @return {object}
+ */
+export function rootReducer(state, action) {
+  return state;
+}
